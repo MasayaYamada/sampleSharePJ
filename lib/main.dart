0cbaf7e22be841412855950d0719d19cc8a64763
MasayaYamada <masaya.yamada.stripes@gmail.com>
2021-07-13 16:30:13 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:share/share.dart';
+
+import 'image_previews.dart';
+
+void main() {
+  runApp(DemoApp());
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  DemoAppState createState() => DemoAppState();
+}
+
+class DemoAppState extends State<DemoApp> {
+  String text = '';
+  String subject = '';
+  List<String> imagePaths = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Share Plugin デモ',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Share plugin デモ'),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'タイトル',
+                    ),
+                    maxLines: 2,
+                    onChanged: (String value) => setState(() {
+                      subject = value;
+                    }),
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'テキスト',
+                    ),
+                    maxLines: 2,
+                    onChanged: (String value) => setState(() {
+                      text = value;
+                    }),
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 12.0)),
+                  ImagePreviews(imagePaths, onDelete: _onDeleteImage),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("Add image"),
+                    onTap: () async {
+                      final imagePicker = ImagePicker();
+                      final pickedFile = await imagePicker.getImage(
+                        source: ImageSource.gallery,
+                      );
+                      if (pickedFile != null) {
+                        setState(() {
+                          imagePaths.add(pickedFile.path);
+                        });
+                      }
+                    },
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 12.0)),
+                  Builder(
+                    builder: (BuildContext context) {
+                      return ElevatedButton(
+                        child: const Text('シェア'),
+                        onPressed: text.isEmpty && imagePaths.isEmpty
+                            ? null
+                            : () => _onShare(context),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+
+  _onDeleteImage(int position) {
+    setState(() {
+      imagePaths.removeAt(position);
+    });
+  }
+
+  _onShare(BuildContext context) async {
+    final RenderBox box = context.findRenderObject() as RenderBox;
+
+    if (imagePaths.isNotEmpty) {
+      await Share.shareFiles(imagePaths,
+          text: text,
+          subject: subject,
+          sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+    } else {
+      await Share.share(text,
+          subject: subject,
+          sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+    }
+  }
+}
